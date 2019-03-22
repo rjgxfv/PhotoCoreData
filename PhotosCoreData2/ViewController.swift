2d72e4e3be837d02c99f274adbdfe9549c32a077
Robert <robert@roberts-macbook-pro.local>
2019-03-22 18:31:22 -0500

    Initial Commit

---
+++ PhotosCoreData2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosCoreData2
+//
+//  Created by Robert on 3/22/19.
+//  Copyright © 2019 Robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
